1804fe3944d2f702a68b65eb5def253e3ae93a35
xyntechx <xyntechx@gmail.com>
2024-08-21 17:12:42 -0700

    Init algo

---
+++ macro_learning.py
@@ -0,0 +1,5 @@
+# Best first search for macro learning, guided by lowest effect size heuristic (+ sequence length according to Algo 1 in thesis)
+from cam.domains.cube.cubeenv import CubeEnv
+
+simulator = CubeEnv()
+simulator.render()
